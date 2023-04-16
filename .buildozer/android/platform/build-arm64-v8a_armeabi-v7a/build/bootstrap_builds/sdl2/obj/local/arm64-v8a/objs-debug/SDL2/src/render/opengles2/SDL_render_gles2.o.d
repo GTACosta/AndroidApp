6ee46ecca2031a1fa2ff100051c37580c952e54a
wyche <gabrieltorres570@gmail.com>
2023-04-16 10:09:06 -0300

    Update of the app

---
+++ .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2/src/render/opengles2/SDL_render_gles2.o.d
@@ -0,0 +1,161 @@
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2/src/render/opengles2/SDL_render_gles2.o: \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/SDL_render_gles2.c \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../SDL_internal.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../dynapi/SDL_dynapi.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_assert.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_log.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_hints.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/SDL_sysvideo.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/../SDL_internal.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_messagebox.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_video.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_pixels.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_endian.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rect.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_error.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rwops.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_surface.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_blendmode.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_shape.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_thread.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_atomic.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mutex.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_metal.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/SDL_vulkan_internal.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vulkan.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vk_platform.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vulkan_core.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vulkan_android.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_vulkan.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_opengles2.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../SDL_sysrender.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_render.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_events.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keyboard.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keycode.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_scancode.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mouse.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_joystick.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_guid.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gamecontroller.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_sensor.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_quit.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gesture.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_touch.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../SDL_yuv_sw_c.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/SDL_blit.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_cpuinfo.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/SDL_shaders_gles2.h \
+  /home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/SDL_gles2funcs.h
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../SDL_internal.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../dynapi/SDL_dynapi.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_assert.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_log.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_hints.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/SDL_sysvideo.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/../SDL_internal.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_messagebox.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_video.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_pixels.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_endian.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rect.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_error.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rwops.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_surface.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_blendmode.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_shape.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_thread.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_atomic.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mutex.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_metal.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/SDL_vulkan_internal.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vulkan.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vk_platform.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vulkan_core.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/./khronos/vulkan/vulkan_android.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_vulkan.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_opengles2.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../SDL_sysrender.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_render.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_events.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keyboard.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keycode.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_scancode.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mouse.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_joystick.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_guid.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gamecontroller.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_sensor.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_quit.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gesture.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_touch.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../SDL_yuv_sw_c.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/../../video/SDL_blit.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_cpuinfo.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/SDL_shaders_gles2.h:
+
+/home/wyche/PycharmProjects/AppProject/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/opengles2/SDL_gles2funcs.h:
